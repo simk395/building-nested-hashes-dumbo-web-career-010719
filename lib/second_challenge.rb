ae2788bc638a88e839f67c6fcd0504225ce76f97
Simon Kong <simon.kong95@gmail.com>
2018-12-16 02:20:30 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,7 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      patriach:{},
+      patriarch:{},
       matriarch:{},
       hero:{},
       hero_friends:[]
@@ -11,7 +11,7 @@ def second_challenge
       patriach:{},
       matriarch:{},
       hero:{},
-      hero_friends:[]
+      heroine_friends:[]
       }}
 
 
